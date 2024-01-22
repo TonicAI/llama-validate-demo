25f8af937b90300e98e02dbb6cab2290e174070d
ethan-tonic <ephilpott@tonic.ai>
2024-01-22 13:18:02 -0500

    Switch to postgres vector

--- app/engine/generate.py
+++ app/engine/generate.py
@@ -1,16 +1,17 @@
-import logging
-
 from dotenv import load_dotenv
 
-from app.engine.constants import DATA_DIR, STORAGE_DIR
+load_dotenv()
+import logging
+
+from app.engine.constants import DATA_DIR
 from app.engine.context import create_service_context
+from app.engine.utils import init_pg_vector_store_from_env
 
 from llama_index import (
     SimpleDirectoryReader,
     VectorStoreIndex,
+    StorageContext,
 )
-
-load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -20,12 +21,18 @@ def generate_datasource(service_context):
     logger.info("Creating new index")
     # load the documents and create the index
     documents = SimpleDirectoryReader(DATA_DIR).load_data()
-    index = VectorStoreIndex.from_documents(documents, service_context=service_context)
-    # store it for later
-    index.storage_context.persist(STORAGE_DIR)
-    logger.info(f"Finished creating new index. Stored in {STORAGE_DIR}")
+    store = init_pg_vector_store_from_env()
+    storage_context = StorageContext.from_defaults(vector_store=store)
+    VectorStoreIndex.from_documents(
+        documents,
+        service_context=service_context,
+        storage_context=storage_context,
+        show_progress=True,  # this will show you a progress bar as the embeddings are created
+    )
+    logger.info(
+        f"Successfully created embeddings in the PG vector store, schema={store.schema_name} table={store.table_name}"
+    )
 
 
 if __name__ == "__main__":
-    service_context = create_service_context()
-    generate_datasource(service_context)
+    generate_datasource(create_service_context())
