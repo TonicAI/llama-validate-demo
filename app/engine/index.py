92b36beb061e2ad748d3fea396134e12c0f0158e
ethan-tonic <ephilpott@tonic.ai>
2024-01-22 13:57:46 -0500

    Revert "Switch to postgres vector"
    This reverts commit 25f8af937b90300e98e02dbb6cab2290e174070d.

--- app/engine/index.py
+++ app/engine/index.py
@@ -1,16 +1,25 @@
 import logging
+import os
 from llama_index import (
-    VectorStoreIndex,
+    StorageContext,
+    load_index_from_storage,
 )
+
+from app.engine.constants import STORAGE_DIR
 from app.engine.context import create_service_context
-from app.engine.utils import init_pg_vector_store_from_env
 
 
 def get_chat_engine():
     service_context = create_service_context()
+    # check if storage already exists
+    if not os.path.exists(STORAGE_DIR):
+        raise Exception(
+            "StorageContext is empty - call 'python app/engine/generate.py' to generate the storage first"
+        )
     logger = logging.getLogger("uvicorn")
-    logger.info("Connecting to index from PGVector...")
-    store = init_pg_vector_store_from_env()
-    index = VectorStoreIndex.from_vector_store(store, service_context)
-    logger.info("Finished connecting to index from PGVector.")
-    return index.as_chat_engine(similarity_top_k=5)
+    # load the existing index
+    logger.info(f"Loading index from {STORAGE_DIR}...")
+    storage_context = StorageContext.from_defaults(persist_dir=STORAGE_DIR)
+    index = load_index_from_storage(storage_context, service_context=service_context)
+    logger.info(f"Finished loading index from {STORAGE_DIR}")
+    return index.as_chat_engine()
